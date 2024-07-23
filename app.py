8e312c87dede26f9270a5dc188744d83266df1b0
FatemehHabibimoghaddam <f.habibimoghaddam@gmail.com>
2024-07-23 10:35:07 +0000

    Second commit for blame

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ def process_data(data):
     result = data.upper()  # Convert data to uppercase
     return result
 
+
+def process_data(data):
+    results = data.upper()  # Converting data to uppercase
+    return results
+
